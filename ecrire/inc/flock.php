6d759571905dc1fb6ce49421ec7acf0592dc9e7a
marcimat <marcimat@rezo.net>
2012-01-01 15:59:22 +0000

    Encore un @ oublié dans supprimer_repertoire().
    git-svn-id: svn://trac.rezo.net/spip/spip@18865 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/flock.php
+++ ecrire/inc/flock.php
@@ -291,7 +291,7 @@ function spip_unlink($f) {
  */
 function supprimer_repertoire($dir) {
 	if (!file_exists($dir)) return true;
-	if (!is_dir($dir) || is_link($dir)) return unlink($dir);
+	if (!is_dir($dir) || is_link($dir)) return @unlink($dir);
 	
 	foreach (scandir($dir) as $item) {
 		if ($item == '.' || $item == '..') continue;
