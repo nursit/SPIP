54b41ec42ec96e59e78c5f38d0f9a453a2cb86f4
marcimat <marcimat@rezo.net>
2012-07-13 15:31:03 +0000

    Commentaire pour décrire un problème à résoudre concernant collecter_requests() et objet_instituer(), en attendant...
    git-svn-id: svn://trac.rezo.net/spip/spip@19713 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/action/editer_objet.php
+++ ecrire/action/editer_objet.php
@@ -116,6 +116,8 @@ function objet_modifier($objet, $id, $set=null) {
 		return $err;
 
 	// Modification de statut, changement de rubrique ?
+	// [fixme] Ici lorsqu'un $set est passé, la fonction collecter_requests() retourne tout
+	//         le tableau $set hors black liste, mais du coup on a possiblement des champs en trop. 
 	$c = collecter_requests(array($champ_date, 'statut', 'id_parent'),array(),$set);
 	$err = objet_instituer($objet, $id, $c);
 
