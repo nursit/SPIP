c12ebd1bbd28a43cc295784b0d14b8a22813c15b
salvatore <salvatore@rezo.net>
2012-08-15 01:37:39 +0000

    langues (public)
    git-svn-id: svn://trac.rezo.net/spip/spip@19814 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/lang/public_nl.php
+++ ecrire/lang/public_nl.php
@@ -8,7 +8,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'accueil_site' => 'Beginpagina', # MODIF
+	'accueil_site' => 'Homepagina',
 	'article' => 'Artikel',
 	'articles' => 'Artikels',
 	'articles_auteur' => 'Artikels van deze auteur',
