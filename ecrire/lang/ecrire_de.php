736c43b42b6fe5c8ebec1db543efafeca3c490e0
salvatore <salvatore@rezo.net>
2012-10-08 01:46:46 +0000

    langues (ecrire)
    git-svn-id: svn://trac.rezo.net/spip/spip@19952 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/lang/ecrire_de.php
+++ ecrire/lang/ecrire_de.php
@@ -841,7 +841,7 @@ in einer Farbe, die den Status anzeigt :',
 	'texte_enrichir_mise_a_jour' => 'Sie können Ihren Text ansprechend gestalten, indem Sie die typografischen Abkürzungen verwenden.',
 	'texte_fichier_authent' => '<b>Soll SPIP die Dateien <tt>.htpasswd</tt> und <tt>.htpasswd-admin</tt> im Verzeichnis @dossier@ anlegen?</b><p>
  Mit diesen Dateien können Sie den Zugang zu anderen Bereichen Ihrer Website (z.B. externe Logfile-Auswertungen) auf Redakteure und Administratoren beschränken.</p><p>Wenn Sie diese Möglickeit nicht nutzen, könnn Sie hier die Grundeinstellung (Dateien werden nicht angelegt) beibehalten.</p>',
-	'texte_informations_personnelles_1' => 'Das System nun persönliche Zugangsdaten für Sie anlegen.',
+	'texte_informations_personnelles_1' => 'Das System wird nun persönliche Zugangsdaten für Sie anlegen.',
 	'texte_informations_personnelles_2' => '(Hinweis: Wenn Sie die Site erneut installieren, und Ihre Zugangsdaten funktionieren, können Sie',
 	'texte_introductif_article' => '(Einleitender Text des Artikels.)',
 	'texte_jeu_caractere' => 'Es empfiehlt sich, das Universalalphabet (<tt>utf-8</tt>) für ihre Website zu verwenden. Es ermöglicht die Darstellung der Zeichen aller Sprachen und wird nunmehr von allen modernen Webbrowsern unterstützt.',
